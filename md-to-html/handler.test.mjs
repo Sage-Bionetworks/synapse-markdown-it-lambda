d9b3495eca0fc5c508476917aa9ea259fc72cd91
Xavier Schildwachter <x.schildwachter@sagebase.org>
2025-06-28 20:54:00 -0700

    Changes for direct invoke + fixes

--- md-to-html/handler.test.mjs
+++ md-to-html/handler.test.mjs
@@ -6,32 +6,25 @@ describe('Handler tests', () => {
   test('missing event', async () => {
     const response = await handler(null, {});
     expect(response.statusCode).toBe(400);
-    expect(response.body).toBe(JSON.stringify({ result: "Missing event or body" }));
-  });
-
-  test('missing body', async () => {
-    const event = { 'notBody': '{ "notMarkdown": "someValue" }' };
-    const response = await handler(event, {});
-    expect(response.statusCode).toBe(400);
-    expect(response.body).toBe(JSON.stringify({ result: "Missing event or body" }));
+    expect(response.body).toBe(JSON.stringify({ result: "Missing event or markdown" }));
   });
 
   test('missing markdown - json', async () => {
-    const event = { 'body': '{ "notMarkdown": "someValue" }' };
+    const event = {"notMarkdown": "someValue" };
     const response = await handler(event, {});
     expect(response.statusCode).toBe(400);
-    expect(response.body).toBe(JSON.stringify({ result:"Missing markdown in body" }));
+    expect(response.body).toBe(JSON.stringify({ result:"Missing event or markdown" }));
   });
 
   test('invalid output type', async () => {
-    const event = { body: JSON.stringify({ markdown: '# Test', output: 'xml' }) };
+    const event = { markdown: "# Test", "output": "xml" };
     const response = await handler(event, {});
     expect(response.statusCode).toBe(400);
     expect(response.body).toBe(JSON.stringify({ result: "Invalid output type" }));
   });
 
   test('valid', async () => {
-    const event = { body: JSON.stringify({ markdown: '# Test', output: 'plain' }) };
+    const event = { markdown: "# Test", output: "plain" };
     const response = await handler(event, {});
     expect(response.statusCode).toBe(200);
     expect(response.body).toBe(JSON.stringify({ result: "TEST" }));
