2a51974c3517c51eb000bc676c96edd2d41322d0
Xavier Schildwachter <x.schildwachter@sagebase.org>
2025-07-09 22:49:26 -0700

    Pulled from test lambda + test fixes

--- md-to-html/core.test.mjs
+++ md-to-html/core.test.mjs
@@ -1,20 +1,6 @@
-import { processMarkdown, genResponse } from './core.mjs';
+import { processMarkdown } from './core.mjs';
 
 describe('core tests', () => {
-
-  test('genResponse - success', () => {
-    const response = genResponse(200, 'Test Success');
-    expect(response).toEqual({
-      statusCode: 200,
-      headers: {
-        "X-Powered-By": "Sage Bionetworks Synapse",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        result: 'Test Success',
-      }),
-    });
-  });
 
   test('header - html', () => {
     const result = processMarkdown("## Hello Markdown!", "html");
--- md-to-html/handler.test.mjs
+++ md-to-html/handler.test.mjs
@@ -1,33 +1,24 @@
 import { handler } from './index.mjs';
-import { processMarkdown, genResponse } from './core.mjs';
+import { processMarkdown } from './core.mjs';
 
 describe('Handler tests', () => {
 
   test('missing event', async () => {
-    const response = await handler(null, {});
-    expect(response.statusCode).toBe(400);
-    expect(response.body).toBe(JSON.stringify({ result: "Missing event or markdown" }));
+    await expect(handler(null, {})).rejects.toThrow('Missing event or markdown');
   });
 
   test('missing markdown - json', async () => {
-    const event = {"notMarkdown": "someValue" };
-    const response = await handler(event, {});
-    expect(response.statusCode).toBe(400);
-    expect(response.body).toBe(JSON.stringify({ result:"Missing event or markdown" }));
+    await expect(handler({"notMarkdown": "someValue" }, {})).rejects.toThrow('Missing event or markdown');
   });
 
   test('invalid output type', async () => {
-    const event = { markdown: "# Test", "output": "xml" };
-    const response = await handler(event, {});
-    expect(response.statusCode).toBe(400);
-    expect(response.body).toBe(JSON.stringify({ result: "Invalid output type" }));
+    await expect(handler({ markdown: "# Test", "output": "xml" }, {})).rejects.toThrow('Invalid output type');
   });
 
   test('valid', async () => {
     const event = { markdown: "# Test", output: "plain" };
     const response = await handler(event, {});
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toBe(JSON.stringify({ result: "TEST" }));
+    expect(response.result).toBe("TEST");
   });
 
 });
